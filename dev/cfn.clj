345f7944543afec99c6fc5b7df570e4f5022964e
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2018-10-18 17:21:39 +0200

    Validate references

--- dev/cfn.clj
+++ dev/cfn.clj
@@ -1,5 +1,6 @@
 (ns cfn
-  (:require [clj-yaml.core :as yaml])
+  (:require [clj-yaml.core :as yaml]
+            [clojure.walk :as walk])
   (:import [org.yaml.snakeyaml Yaml TypeDescription DumperOptions DumperOptions$FlowStyle]
            [org.yaml.snakeyaml.constructor Constructor]
            [org.yaml.snakeyaml.representer Representer BaseRepresenter]
@@ -43,10 +44,38 @@
     (.addTypeDescription yaml (make-type-description Ref))
     yaml))
 
+(defn find-refs [template]
+  (let [refs (atom [])]
+    (walk/postwalk #(when (or (= cfn.Sub (type %))
+                              (= cfn.Ref (type %)))
+                      (swap! refs conj %)
+                      %)
+                   template)
+    @refs))
+
+(defn validate-references [cfn]
+  (let [referrables (set (concat (->> cfn :Parameters keys (map name))
+                                 (->> cfn :Resources keys (map name))))
+        references (set (->> (find-refs cfn)
+                             (keep (fn [x]
+                                     (condp = (type x)
+                                       cfn.Ref [(:value x)]
+                                       cfn.Sub (->> (re-seq #"\$\{([^\}]+)" (:value x))
+                                                    (map second)
+                                                    (filter #(not (.contains % "::")))))))
+                             (apply concat)))
+        unresolved-references (clojure.set/difference references referrables)]
+    (when-not (empty? unresolved-references)
+      (throw (ex-info (str "Unresolved references found: " unresolved-references)
+                      {:unresolved unresolved-references})))))
+
 (defn parse [cfn-yml]
-  (yaml/decode (.load (make-yaml) cfn-yml) true))
+  (let [cfn (yaml/decode (.load (make-yaml) cfn-yml) true)]
+    (validate-references cfn)
+    cfn))
 
 (defn generate-string [cfn-data]
+  (validate-references cfn-data)
   (.dump (make-yaml) (yaml/encode cfn-data)))
 
 (defn load-and-print [filename]
