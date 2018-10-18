49542486aa60c3cd3737531a3dc696512e4d1b6e
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2018-10-18 17:42:54 +0200

    Derive API GW paths from ion-config

--- dev/user.clj
+++ dev/user.clj
@@ -1,11 +1,32 @@
 (ns user
   (:require [cider-nrepl.main :as nrepl]
             [clojure.java.io :as io]
+            [clojure.edn :as edn]
             [cfn]
             [amazonica.aws.cloudformation :as aws-cfn]))
 
 (defn start-nrepl-server []
   (nrepl/init ["cider.nrepl/cider-middleware"]))
+
+(defn make-paths [datomic-config]
+  (let [api-lambdas (keep (fn [[k v]]
+                       (when (= :api-gateway/proxy (:integration v))
+                         k))
+                     (:lambdas datomic-config))]
+    (into {} (for [lambda api-lambdas]
+               [(str "/" (name lambda))
+                {:get
+                 {:produces ["application/json"]
+                  :responses {}
+                  :x-amazon-apigateway-integration
+                  {:uri #cfn.Sub{:value (str "arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${AWS::Region}:${AWS::AccountId}:function:${ComputeGroup}-"
+                                             (name lambda)
+                                             "/invocations")}
+                   :responses {:default {:statusCode "200"}}
+                   :passthroughBehavior "when_no_match"
+                   :httpMethod "POST"
+                   :contentHandling "CONVERT_TO_TEXT"
+                   :type "aws_proxy"}}}]))))
 
 (def template
   {:AWSTemplateFormatVersion "2010-09-09"
@@ -21,18 +42,7 @@
        :info {:title "ShoppingList"}
        :basePath "/dev"
        :schemes ["https"]
-       :paths
-       {"/get-items"
-        {:get
-         {:produces ["application/json"]
-          :responses {}
-          :x-amazon-apigateway-integration
-          {:uri #cfn.Sub{:value "arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${AWS::Region}:${AWS::AccountId}:function:${ComputeGroup}-get-items/invocations"}
-           :responses {:default {:statusCode "200"}}
-           :passthroughBehavior "when_no_match"
-           :httpMethod "POST"
-           :contentHandling "CONVERT_TO_TEXT"
-           :type "aws_proxy"}}}}
+       :paths (make-paths (edn/read-string (slurp (io/resource "datomic/ion-config.edn"))))
        :x-amazon-apigateway-binary-media-types ["*/*"]}}}
     :ApiInvokePermission
     {:Type "AWS::Lambda::Permission"
