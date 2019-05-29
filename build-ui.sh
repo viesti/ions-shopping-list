b711043cb7e826e66f2ded6e27d7ce9bdbf0de97
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2019-05-29 23:47:02 +0300

    Remove defunct UI

--- build-ui.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-clojure -m figwheel.main -O advanced -bo cruel
--- src-cljs/shopping_list/core.cljs
+++
@@ -1,9 +0,0 @@
-(ns shopping-list.core
-  (:require [reagent.core :as r]))
-
-(defn main []
-  [:h1 "Moi"])
-
-(r/render [main]
-  (js/document.getElementById "app"))
-
